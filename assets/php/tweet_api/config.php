e39233bbbfe5c215ede83fc46d2744a9e18c460f
ischools <ischoolsdesign@gmail.com>
2015-03-24 15:51:21 -0700

    Initial Commit

---
+++ assets/php/tweet_api/config.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * Your Twitter App Info
+	 Please refet to docs for information about following details
+     */
+    
+    // Consumer Key
+    define('CONSUMER_KEY', 'your Consumer Key');
+    define('CONSUMER_SECRET', 'your Consumer Secret');
+
+    // User Access Token
+    define('ACCESS_TOKEN', 'your Access Token');
+    define('ACCESS_SECRET', 'Your Access Secret');
